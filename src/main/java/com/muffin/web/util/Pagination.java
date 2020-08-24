f673b3fcd33a66687ff1161aece1c1298120d3ef
김희진 <heejiney@gmail.com>
2020-08-25 01:14:32 +0900

    20200825 pagination

--- src/main/java/com/muffin/web/util/Pagination.java
+++ src/main/java/com/muffin/web/util/Pagination.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Lazy
 public class Pagination {
-
     private int listSize, rangeSize, page, range, listCnt, pageCnt, startPage, startList, endPage;
     private boolean prev, next;
 
@@ -19,14 +18,16 @@ public class Pagination {
         this.page = page;
         this.range = range;
         this.listCnt = listCnt;
-        this.pageCnt = (listCnt % listSize != 0) ? (listCnt/listSize + 1) : (listCnt/listSize);;
+        this.pageCnt = (listCnt % listSize != 0) ? (listCnt/listSize + 1) : (listCnt/listSize);
         this.startPage = (range-1) * rangeSize + 1;
         this.endPage = range * rangeSize;
         this.startList = (page - 1) * listSize;
         this.prev = range != 1;
         this.next = endPage <= pageCnt;
-        if(this.endPage > this.pageCnt){
-            this.endPage = this.pageCnt;
+
+        if(this.endPage >= this.pageCnt){
+            this.next = false;
         }
     }
+
 }
